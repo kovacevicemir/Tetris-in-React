4cc7c9f717bf6caea196c4624d31d4f1572fecd3
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-11 23:37:14 +1000

    draw first teromino

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { createStage } from '../gameHelpers'
 
-export const useStage = () =>{
+export const useStage = (player, resetPlayer) =>{
     const [stage, setStage] = useState(createStage())
+
+    useEffect(() => {
+        const updateStage = prevStage =>{
+            //Clear stage from previous render
+            const newStage = prevStage.map(row=>
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+            )
+            
+            //Then draw the tetromino
+            player.tetromino.forEach((row, y) => {
+                player.tetromino.forEach((row, y) => {
+                    row.forEach((value, x) => {
+                        if(value !== 0){
+                            newStage[y + player.pos.y][x + player.pos.x] = [
+                                value,
+                                `${player.collided ? 'merged' : 'clear'}`
+                            ]
+                        }
+                    })
+                })
+            })
+
+            return newStage
+
+        }
+
+
+        setStage(prev => updateStage(prev))
+    }, [])
 
     return [stage, setStage]
 }
+
+//1:32:30
